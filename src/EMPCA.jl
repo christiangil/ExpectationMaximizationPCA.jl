b9302b5681647c9a74d40bbda8d853ff61e9f9cb
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2021-04-29 12:28:12 -0400

    deleting unnecessary line

--- src/EMPCA.jl
+++ src/EMPCA.jl
@@ -6,7 +6,6 @@ __precompile__()
 module EMPCA
     using PyCall
     const empca = PyNULL()
-    # joinpath(pathof(EMPCA),"src")
     function __init__()
         pushfirst!(PyVector(pyimport("sys")."path"), joinpath(pathof(EMPCA),".."))
         copy!(empca, pyimport("empca"))
